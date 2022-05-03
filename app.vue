560a8a353ea2922be84a801fcabb02afd66f7664
BestTempuraJP <degutech@gmail.com>
2022-05-03 10:11:13 +0900

    create nuxt3 app

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{ number }}</p>
+    <button @click="inc()">
+      Inc
+    </button>
+    <button @click="dec()">
+      Dec
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+let number = ref(1)
+
+const inc = () => {
+  number.value++
+}
+const dec = () => {
+  number.value--
+}
+
+</script>
