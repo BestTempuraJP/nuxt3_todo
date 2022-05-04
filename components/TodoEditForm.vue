dd8d50cf8154d750cc84f6f2b74a975e7675af47
BestTempuraJP <degutech@gmail.com>
2022-05-04 22:22:43 +0900

    feat: 編集成功時にアラートを表示

--- components/TodoEditForm.vue
+++ components/TodoEditForm.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-card class="pa-5 mb-7">
-    <v-form @submit="handleSubmit">
-      <h3 class="subtitle-2 pb-3">
-        Edit Task
-      </h3>
-      <v-text-field v-model="title" :error-messages="titleError" label="Title" variant="outlined" />
-      <v-text-field v-model="body" :error-messages="bodyError" label="Body" variant="outlined" />
-      <v-btn color="warning" type="submit">
-        Edit
-      </v-btn>
-    </v-form>
-  </v-card>
+  <div>
+    <v-card class="pa-5 mb-7">
+      <v-form @submit="handleSubmit">
+        <h3 class="subtitle-2 pb-3">
+          Edit Task
+        </h3>
+        <v-text-field v-model="title" :error-messages="titleError" label="Title" variant="outlined" />
+        <v-text-field v-model="body" :error-messages="bodyError" label="Body" variant="outlined" />
+        <v-btn color="warning" type="submit">
+          Edit
+        </v-btn>
+      </v-form>
+    </v-card>
+    <v-alert v-show="success" type="success" class="mb-5">Successfully updated!</v-alert>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -40,18 +43,27 @@ resetForm({
   }
 })
 
+const success = ref(false)
 const { updateTodo } = useTodos()
 const handleSubmit = async (e: Event) => {
   const result = await validate()
   if (result.valid) {
-    updateTodo({
+    await updateTodo({
       id: todo.id,
       title: title.value,
       body: body.value
     })
+    success.value = true
     e.preventDefault()
   } else {
     return true
   }
 }
+watch(success, (newVal: boolean) => {
+  if (newVal === true) {
+    setTimeout(() => {
+      success.value = false
+    }, 3000)
+  }
+})
 </script>
