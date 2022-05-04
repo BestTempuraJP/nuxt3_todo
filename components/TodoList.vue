8fea25c80d1da91072bb9aa0fabee7983c7235bc
BestTempuraJP <degutech@gmail.com>
2022-05-04 22:23:04 +0900

    style: fixed v-chip

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -7,13 +7,13 @@
             <p>{{ todo.title }}</p>
           </v-card-title>
           <v-card-text>
-            <v-chip class="mb-2">
+            <v-chip class="mb-2" label color="cyan">
               id: {{ todo.id }}
             </v-chip>
             <p>{{ todo.body }}</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn :to="{name: 'id', params: {id: todo.id}}" color="secondary">
+            <v-btn :to="{name: 'id', params: {id: todo.id}}" color="warning">
               EDIT
             </v-btn>
             <v-btn color="error" @click="drop(todo.id)">
