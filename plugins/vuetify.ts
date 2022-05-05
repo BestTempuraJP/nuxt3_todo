8c359622bc5654139abe6305b77375989d862b13
BestTempuraJP <degutech@gmail.com>
2022-05-06 06:45:51 +0900

    chore: Vuetify 必要なのコンポーネントのみ読み込む

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,10 +1,20 @@
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
+import { VAlert, VBtn, VCard, VCheckbox, VContainer, VForm, VIcon, VTable, VTextField } from 'vuetify/components'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
-    components
+    components: {
+      VAlert,
+      VBtn,
+      VCard,
+      VCheckbox,
+      VContainer,
+      VForm,
+      VIcon,
+      VTable,
+      VTextField
+    }
   })
 
   nuxtApp.vueApp.use(vuetify)
